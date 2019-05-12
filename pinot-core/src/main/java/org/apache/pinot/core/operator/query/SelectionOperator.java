31c0e5abf75b0c0827e6a04561bb074c5ed920d9
kishore gopalakrishna <g.kishore@gmail.com>
2019-05-12 11:36:05 -0700

    Addressing review comments

--- pinot-core/src/main/java/org/apache/pinot/core/operator/query/SelectionOperator.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/query/SelectionOperator.java
@@ -1,19 +1,15 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements.  See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership.  The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the License.  You may obtain
+ * a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied.  See the License for the specific language governing permissions and limitations
  * under the License.
  */
 package org.apache.pinot.core.operator.query;
@@ -196,33 +192,27 @@ public class SelectionOperator extends BaseOperator<IntermediateResultsBlock> {
     int numDocsScanned = 0;
 
     TransformBlock transformBlock;
-    if (_orderByExpressions.isEmpty()) {
-      while ((transformBlock = _transformOperator.nextBlock()) != null) {
-        for (int i = 0; i < _expressions.size(); i++) {
-          TransformExpressionTree expression = _expressions.get(i);
-          _blockValSets[i] = transformBlock.getBlockValueSet(expression);
-        }
-        TransformBlockDataFetcher dataFetcher;
-        dataFetcher = new TransformBlockDataFetcher(_blockValSets, _dictionaries,
-            _expressionResultMetadata);
+    boolean selectionOnly = _orderByExpressions.isEmpty();
+    while ((transformBlock = _transformOperator.nextBlock()) != null) {
+      for (int i = 0; i < _expressions.size(); i++) {
+        TransformExpressionTree expression = _expressions.get(i);
+        _blockValSets[i] = transformBlock.getBlockValueSet(expression);
+      }
+      TransformBlockDataFetcher dataFetcher;
+      dataFetcher = new TransformBlockDataFetcher(_blockValSets, _dictionaries,
+          _expressionResultMetadata);
+      if (!selectionOnly) {
         int docId = 0;
         while (_rowEvents.size() < _limit && docId < transformBlock.getNumDocs()) {
           Serializable[] row = dataFetcher.getRow(docId);
           _rowEvents.add(row);
+          docId = docId + 1;
         }
         if (_rowEvents.size() >= _limit) {
           break;
         }
-      }
-    } else {
-      while ((transformBlock = _transformOperator.nextBlock()) != null) {
-        for (int i = 0; i < _expressions.size(); i++) {
-          TransformExpressionTree expression = _expressions.get(i);
-          _blockValSets[i] = transformBlock.getBlockValueSet(expression);
-        }
-        TransformBlockDataFetcher dataFetcher;
-        dataFetcher = new TransformBlockDataFetcher(_blockValSets, _dictionaries,
-            _expressionResultMetadata);
+      } else {
+        //Selection + orderby
         int docId = 0;
         while (docId < transformBlock.getNumDocs()) {
           Serializable[] row = dataFetcher.getRow(docId);
@@ -233,6 +223,7 @@ public class SelectionOperator extends BaseOperator<IntermediateResultsBlock> {
             _priorityQueue.poll();
             _priorityQueue.offer(row);
           }
+          docId = docId + 1;
         }
       }
     }
