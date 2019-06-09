e419da7ad908c41b3fbd91a5fd7b3b8987648677
Xiang Fu <fx19880617@gmail.com>
2019-06-08 17:47:47 -0700

    Adding isMatch support

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -571,6 +571,28 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
     Assert.assertEquals(val1, val4);
   }
 
+  @Test
+  public void testFilterWithInvertedIndexUDF()
+      throws Exception {
+    int daysSinceEpoch = 16138;
+    long secondsSinceEpoch = 16138 * 24 * 60 * 60;
+
+    String[] origins =  new String[]{"ATL","ORD","DFW","DEN","LAX","IAH","SFO","PHX","LAS","EWR","MCO","BOS","SLC","SEA","MSP","CLT","LGA","DTW","JFK","BWI"};
+    String pqlQuery;
+    for (String origin : origins) {
+      pqlQuery = "SELECT count(*) FROM mytable WHERE Origin = \""+origin+"\" AND DaysSinceEpoch = " + daysSinceEpoch;
+      JsonNode response1 = postQuery(pqlQuery);
+      //System.out.println(response1);
+      pqlQuery = "SELECT count(*) FROM mytable WHERE Origin = \""+origin+"\" AND timeConvert(DaysSinceEpoch,'DAYS','SECONDS') = " + secondsSinceEpoch;
+      JsonNode response2 = postQuery(pqlQuery);
+      //System.out.println(response2);
+      double val1 = response1.get("aggregationResults").get(0).get("value").asDouble();
+      double val2 = response2.get("aggregationResults").get(0).get("value").asDouble();
+      Assert.assertEquals(val1, val2);
+    }
+
+  }
+
   @AfterClass
   public void tearDown()
       throws Exception {
